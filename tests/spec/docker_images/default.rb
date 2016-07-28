250ab14b96c7ed08607743a24b0ac01309ce7b9c
Michael Schaar <kraeml@sesamestreet.franken.de>
2016-07-28 14:52:16 +0200

    Adding failing serverspec test in travis

---
+++ tests/spec/docker_images/default.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+require 'serverspec'
+require 'docker'
+
+images=%w( ubuntu:latest owncloud/ubuntu:latest centos:latest)
+
+images.each do |image|
+  describe image+" image" do
+      before(:all) {
+          @image = Docker::Image.all().detect{|i| i.info['RepoTags'] == [image]}
+      }
+
+      it "should be availble" do
+          expect(@image).to_not be_nil
+      end
+  end
+end
