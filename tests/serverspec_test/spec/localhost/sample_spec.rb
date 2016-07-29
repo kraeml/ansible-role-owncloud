72e8185257d5310c0cbfbf84007da368131a2125
vagrant example <vagrant@example.com>
2016-07-29 07:37:43 +0000

    Travis test with running container

--- tests/serverspec_test/spec/localhost/sample_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper_docker'
-require 'serverspec'
-require 'docker'
-
-describe docker_container 'testcontainer' do
-  it { should exist }
-  it { should be_running }
-end
--- tests/serverspec_test/spec/spec_helper_docker.rb
+++ tests/serverspec_test/spec/spec_helper_docker.rb
@@ -1,4 +1,5 @@
 require 'serverspec'
+require 'docker'
 
 set :backend, :docker
 
