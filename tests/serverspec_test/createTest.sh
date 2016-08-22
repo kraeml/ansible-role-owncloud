c53b3389aa1afc7eee264459b180986a2a4cce23
Michael Schaar <kraeml@sesamestreet.franken.de>
2016-08-22 12:01:08 +0200

    Missing files added

---
+++ tests/serverspec_test/createTest.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+read -n1 -p "Test ownloud-server or ubuntu-16.04? [o,u]: " doit
+case $doit in
+  o|O) distribution=owncloud && version=server;;
+  u|U) distribution=ubuntu && version=16.04;;
+  *) echo dont know ;;
+esac
+
+echo
+echo "Building docker image for testing"
+docker build --rm=true --file=../Dockerfile.${distribution}-${version} --tag=${distribution}-${version}:ansible ../
+
+echo
+echo "Running Testdummy"
+docker run -tdi --name testcontainer ${distribution}-${version}:ansible /bin/bash
+
+echo
+echo "Executing tests:"
+TARGET_CONTAINER=testcontainer TARGET_IMAGE=${distribution}-${version}:ansible rake spec
+
+echo
+echo "Cleaning up"
+docker rm -f testcontainer
